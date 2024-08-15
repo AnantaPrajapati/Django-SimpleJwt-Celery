13e919265c9416039e5d9c5660318caaf6bece9b
AnantaPrajapati <anantaprajapati0@gmail.com>
2024-08-15 17:21:40 +0545

    redis

--- simplejwt/simplejwt/settings.py
+++ simplejwt/simplejwt/settings.py
@@ -182,3 +182,9 @@ CLOUDINARY_STORAGE = {
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+#celery configurations 
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_TIMEZONE = "Asia/Kathmandu"
+
